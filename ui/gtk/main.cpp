63a930dea7049ac9cc638620492b707fa99dceba
Andy McCall <mailme@andymccall.co.uk>
2025-09-20 19:38:12 +0100

    Remove legacy GTK entry: delete ui/gtk/main.cpp

--- ui/gtk/main.cpp
+++
@@ -1,3 +0,0 @@
-// Legacy file intentionally left without an entry point.
-// The GTK application entry point is defined in ui/gtk/app.cpp.
-// This file remains only to avoid accidental resurrection of the old monolithic UI.
